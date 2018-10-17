a34ee66c07bf12b7cc099812e300e52e8d11d8ef
kimk <38691828+claudiact6@users.noreply.github.com>
2018-10-16 20:01:12 -0500

    spotify working

--- liri.js
+++ liri.js
@@ -5,34 +5,39 @@ var keys = require("./keys");
 var Spotify = require('node-spotify-api')
 var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
-console.log(command);
+console.log("command:", command);
 var searchTerm = "";
-for (i = 3; i < process.argv.length; i++) {
-    searchTerm += process.argv[i] + "%20";
+
+//If there is a search term, assign it to a variable
+if (process.argv.length > 3) {
+    for (i = 3; i < process.argv.length - 1; i++) {
+        searchTerm += process.argv[i] + "%20";
+    }
+    searchTerm += process.argv[process.argv.length - 1];
 }
 
 //What to do if searchTerm is blank for commands that require a searchTerm
-if(command === "movie-this" && searchTerm === "") {
-        searchTerm = "Mr. Nobody";
-} else if(command === "spotify-this-song" && searchTerm === "") {
-        searchTerm = "I Saw the Sign";
+if (command === "movie-this" && searchTerm === "") {
+    searchTerm = "Mr. Nobody";
+} else if (command === "spotify-this-song" && searchTerm === "") {
+    searchTerm = "I Saw the Sign";
 }
 
-console.log(searchTerm);
+console.log("search term:", searchTerm);
 takeCommand(command, searchTerm);
 
 function takeCommand(command, searchTerm) {
-    switch(command) {
+    switch (command) {
         case "concert-this":
             //Search BandsInTown API for artist to provide venue name, location, and date of all upcoming concerts.
-            var queryUrl = "https://rest.bandsintown.com/artists/Brandi%20Carlile/events?app_id=codingbootcamp"
+            var queryUrl = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp"
             console.log(queryUrl);
             request(queryUrl, function (error, response, body) {
                 // If the request is successful
                 if (!error && response.statusCode === 200) {
                     var concert = JSON.parse(body);
                     console.log("---Upcoming concerts---")
-                    for(i = 0; i < concert.length; i++) {
+                    for (i = 0; i < concert.length; i++) {
                         console.log(concert[i].lineup + " at " + concert[i].venue.name);
                         console.log(concert[i].datetime);
                         console.log(concert[i].venue.city + ", " + concert[i].venue.region);
@@ -41,9 +46,22 @@ function takeCommand(command, searchTerm) {
             });
             break;
         case "spotify-this-song":
-            //Search the Spotify API to show song name, artist(s), the album, and a link to the song from Spotify
+            //Search Spotify for the song and return song name, artist(s), preview link of the song, and the album that the song is from
+            console.log("searching for:",searchTerm)
+            spotify.search({ type: 'track', query: "I%20saw%20the%20sign", limit: 1 }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                var song = data.tracks.items[0];
+                console.log(song);
+                console.log("---" + song.name + "---");
+                console.log("As sung by:",song.artists[0].name);
+                console.log("On the album:",song.album.name);
+                console.log("Preview it at:",song.preview_url);
+
+            });
             break;
-        case "movie-this":            
+        case "movie-this":
             //Search the OMDB API to show movie title, release year, IMDB rating, Rotten Tomatoes rating, country where the movie was produced, language of the movie, plot summary, and actors.
             var queryUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy";
             request(queryUrl, function (error, response, body) {
@@ -57,7 +75,7 @@ function takeCommand(command, searchTerm) {
                     console.log("Produced in", movie.Country);
                     console.log("Language:", movie.Language);
                     console.log("Leading actors:", movie.Actors);
-                    console.log("Summary:",movie.Plot);
+                    console.log("Summary:", movie.Plot);
                 }
             });
             break;
@@ -77,8 +95,8 @@ function takeCommand(command, searchTerm) {
 
 //Log all the search results to log.txt
 function logData() {
-    fs.writeFile("log.txt", data, function(err) {
-        if(err) {
+    fs.writeFile("log.txt", data, function (err) {
+        if (err) {
             console.log("Oops, there was an error");
         } else {
             console.log("Data was logged to file!")
